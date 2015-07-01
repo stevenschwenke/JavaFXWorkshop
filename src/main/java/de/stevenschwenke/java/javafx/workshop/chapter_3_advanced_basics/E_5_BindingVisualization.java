f7d195c585e045d4bf3d0801afb146fa8d8ebde6
stevenschwenke <steven@stevenschwenke.de>
2015-07-01 12:38:38 +0200

    New implementation of Binding Visualization

--- src/main/java/de/stevenschwenke/java/javafx/workshop/chapter_3_advanced_basics/E_5_BindingVisualization.java
+++ src/main/java/de/stevenschwenke/java/javafx/workshop/chapter_3_advanced_basics/E_5_BindingVisualization.java
@@ -26,10 +26,10 @@ public class E_5_BindingVisualization extends Application implements Initializab
     private TextField t2;
     @FXML
     private TextField t3;
-    @FXML
-    private TextField t4;
 
-    /** Binds 1 on 2 and is placed right before 2. */
+    /**
+     * Binds 1 on 2 and is placed right before 2.
+     */
     @FXML
     private Circle c_1_to_2;
     @FXML
@@ -39,15 +39,11 @@ public class E_5_BindingVisualization extends Application implements Initializab
     @FXML
     private Circle c_3_to_2;
     @FXML
-    private Circle c_3_to_4;
+    private Circle c_3_to_1;
     @FXML
-    private Circle c_4_to_3;
-    @FXML
-    private Circle c_4_to_1;
-    @FXML
-    private Circle c_1_to_4;
+    private Circle c_1_to_3;
 
-    private SimpleBooleanProperty t1BoundToT2, t2BoundToT1, t2BoundToT3, t3BoundToT2, t3BoundToT4, t4BoundToT3, t4BoundToT1, t1BoundToT4;
+    private SimpleBooleanProperty t1BoundToT2, t2BoundToT1, t2BoundToT3, t3BoundToT2, t3BoundToT1, t1BoundToT3;
 
     @FXML
     public void click1To2() {
@@ -73,6 +69,18 @@ public class E_5_BindingVisualization extends Application implements Initializab
         System.out.println("t3BoundTo2 now " + t3BoundToT2.get());
     }
 
+    @FXML
+    public void click3To1() {
+        t3BoundToT1.set(!t3BoundToT1.get());
+        System.out.println("t3BoundTo1 now " + t3BoundToT1.get());
+    }
+
+    @FXML
+    public void click1To3() {
+        t1BoundToT3.set(!t1BoundToT3.get());
+        System.out.println("t1BoundTo3 now " + t1BoundToT3.get());
+    }
+
     @Override
     public void start(Stage stage) throws Exception {
 
@@ -91,102 +99,96 @@ public class E_5_BindingVisualization extends Application implements Initializab
     public void initialize(URL arg0, ResourceBundle arg1) {
         t1BoundToT2 = new SimpleBooleanProperty(false);
         t1BoundToT2.addListener(observable -> {
-
             resetBindings();
-
-            if (((SimpleBooleanProperty) observable).get()) {
-                c_1_to_2.setFill(Color.DODGERBLUE);
-            } else {
-                c_1_to_2.setFill(Color.LIGHTGRAY);
-            }
+            c_1_to_2.setFill(t1BoundToT2.getValue() ? Color.DODGERBLUE : Color.LIGHTGRAY);
         });
         t2BoundToT1 = new SimpleBooleanProperty(false);
         t2BoundToT1.addListener(observable -> {
-
             resetBindings();
-
-            if (((SimpleBooleanProperty) observable).get()) {
-                c_2_to_1.setFill(Color.DODGERBLUE);
-            } else {
-                c_2_to_1.setFill(Color.LIGHTGRAY);
-            }
+            c_2_to_1.setFill(t2BoundToT1.getValue() ? Color.DODGERBLUE : Color.LIGHTGRAY);
         });
         t2BoundToT3 = new SimpleBooleanProperty(false);
         t2BoundToT3.addListener(observable -> {
-
             resetBindings();
-
-            if (((SimpleBooleanProperty) observable).get()) {
-                c_2_to_3.setFill(Color.DODGERBLUE);
-            } else {
-                c_2_to_3.setFill(Color.LIGHTGRAY);
-            }
+            c_2_to_3.setFill(t2BoundToT3.getValue() ? Color.DODGERBLUE : Color.LIGHTGRAY);
         });
         t3BoundToT2 = new SimpleBooleanProperty(false);
         t3BoundToT2.addListener(observable -> {
-
             resetBindings();
-
-            if (((SimpleBooleanProperty) observable).get()) {
-                c_3_to_2.setFill(Color.DODGERBLUE);
-            } else {
-                c_3_to_2.setFill(Color.LIGHTGRAY);
-            }
+            c_3_to_2.setFill(t3BoundToT2.getValue() ? Color.DODGERBLUE : Color.LIGHTGRAY);
         });
 
+        t3BoundToT1 = new SimpleBooleanProperty(false);
+        t3BoundToT1.addListener(observable -> {
+            resetBindings();
+            c_3_to_1.setFill(t3BoundToT1.getValue() ? Color.DODGERBLUE : Color.LIGHTGRAY);
+        });
+        t1BoundToT3 = new SimpleBooleanProperty(false);
+        t1BoundToT3.addListener(observable -> {
+            resetBindings();
+            c_1_to_3.setFill(t1BoundToT3.getValue() ? Color.DODGERBLUE : Color.LIGHTGRAY);
+        });
     }
 
     private void resetBindings() {
         t1.textProperty().unbind();
         t1.textProperty().unbindBidirectional(t2.textProperty());
         t1.textProperty().unbindBidirectional(t3.textProperty());
-        t1.textProperty().unbindBidirectional(t4.textProperty());
 
         t2.textProperty().unbind();
         t2.textProperty().unbindBidirectional(t1.textProperty());
         t2.textProperty().unbindBidirectional(t3.textProperty());
-        t2.textProperty().unbindBidirectional(t4.textProperty());
 
         t3.textProperty().unbind();
         t3.textProperty().unbindBidirectional(t1.textProperty());
         t3.textProperty().unbindBidirectional(t2.textProperty());
-        t3.textProperty().unbindBidirectional(t4.textProperty());
 
-        t4.textProperty().unbind();
-        t4.textProperty().unbindBidirectional(t1.textProperty());
-        t4.textProperty().unbindBidirectional(t2.textProperty());
-        t4.textProperty().unbindBidirectional(t3.textProperty());
-
-        if(t1BoundToT2.and(t2BoundToT1).get()) {
+        if (t1BoundToT2.and(t2BoundToT1).get()) {
             t1.textProperty().bindBidirectional(t2.textProperty());
         } else if (t1BoundToT2.get()) {
             t2.textProperty().bind(t1.textProperty());
-        }  else if (t2BoundToT1.get()) {
+        } else if (t2BoundToT1.get()) {
             t1.textProperty().bind(t2.textProperty());
         }
 
-        if(t2BoundToT1.and(t1BoundToT2).get()) {
+        if (t2BoundToT1.and(t1BoundToT2).get()) {
             t2.textProperty().bindBidirectional(t1.textProperty());
         } else if (t2BoundToT1.get()) {
             t1.textProperty().bind(t2.textProperty());
-        }  else if (t1BoundToT2.get()) {
+        } else if (t1BoundToT2.get()) {
             t2.textProperty().bind(t1.textProperty());
         }
 
-        if(t2BoundToT3.and(t3BoundToT2).get()) {
+        if (t2BoundToT3.and(t3BoundToT2).get()) {
             t2.textProperty().bindBidirectional(t3.textProperty());
         } else if (t2BoundToT3.get()) {
             t3.textProperty().bind(t2.textProperty());
-        }  else if (t3BoundToT2.get()) {
+        } else if (t3BoundToT2.get()) {
             t2.textProperty().bind(t3.textProperty());
         }
 
-        if(t3BoundToT2.and(t2BoundToT3).get()) {
+        if (t3BoundToT2.and(t2BoundToT3).get()) {
             t3.textProperty().bindBidirectional(t2.textProperty());
         } else if (t3BoundToT2.get()) {
             t2.textProperty().bind(t3.textProperty());
-        }  else if (t2BoundToT3.get()) {
+        } else if (t2BoundToT3.get()) {
             t3.textProperty().bind(t2.textProperty());
+        }
+
+        if (t3BoundToT1.and(t1BoundToT3).get()) {
+            t3.textProperty().bindBidirectional(t1.textProperty());
+        } else if (t3BoundToT1.get()) {
+            t1.textProperty().bind(t3.textProperty());
+        } else if (t1BoundToT3.get()) {
+            t3.textProperty().bind(t1.textProperty());
+        }
+
+        if (t1BoundToT3.and(t3BoundToT1).get()) {
+            t1.textProperty().bindBidirectional(t3.textProperty()); // TODO WEG
+        } else if (t1BoundToT3.get()) {
+            t3.textProperty().bind(t1.textProperty());
+        } else if (t3BoundToT1.get()) {
+            t1.textProperty().bind(t3.textProperty());
         }
     }
 
