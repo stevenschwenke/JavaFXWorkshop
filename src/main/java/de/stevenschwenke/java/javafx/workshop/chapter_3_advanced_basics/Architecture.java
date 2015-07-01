14c2ed4d03a5a3e6c6f649066f6df89c4e06da7b
stevenschwenke <steven@stevenschwenke.de>
2015-07-01 10:34:38 +0200

    Architecture background

---
+++ src/main/java/de/stevenschwenke/java/javafx/workshop/chapter_3_advanced_basics/Architecture.java
@@ -0,0 +1,30 @@
+package de.stevenschwenke.java.javafx.workshop.chapter_3_advanced_basics;
+
+/**
+ * Created by schwenks on 01.07.2015.
+ */
+public class Architecture {
+
+    /*
+        See Architecture.png:
+
+        Public Layer (top layer):
+            Only use APIs of the public layer!
+
+        Private Layer (Quantum Toolkit):
+            Java, but shouldn't be used by a developer. APIs could change without warning.
+
+        Native Layer (OpenGL, D3D, Glass Window Toolkit, Media Engine, Web Engine, Prism):
+            Not developed in Java.
+            Grant access to native OS layer.
+            Prism for rendering JavaFX views. Several hardware-orientated implementations of Prism, like Direct3D on
+            Windows.
+            Also media- and web-engines.
+            => The native layer is the reason why JavaFX cannot be delivered as a one-JAR framework: native components
+               specific to OS
+
+
+
+     */
+
+}
